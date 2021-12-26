6584e2182957d8ce9f8897e2dafaf71219574c6a
liadmor <liadmor96@gmail.com>
2021-12-26 19:59:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mirrorapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.mirrorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        Animation zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        ImageView img = findViewById(R.id.image);
+        img.startAnimation(zoom);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
